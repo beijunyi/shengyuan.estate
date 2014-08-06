198b650bb7ed3881f74f55e2ebb5c61b45be499a
beijunyi <junyi.bei@formicary.net>
2014-08-06 18:08:46 +0100

    remove unused files preparing for jawr

--- webui/src/main/webapp/scripts/components/GlobalHeaderController.js
+++ webui/src/main/webapp/scripts/components/GlobalHeaderController.js
@@ -1,5 +1,9 @@
 shengyuan.controller('GlobalHeaderController', function ($scope, $location) {
   $scope.isActive = function(route) {
     return route === $location.path();
-  }
+  };
+
+  $scope.slides = [{
+
+  }];
 });
